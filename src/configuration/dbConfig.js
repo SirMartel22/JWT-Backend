b5a834551b1c02660978522672bdba9e25f582f4
DevTimmy <oyinloyematthew6@gmail.com>
2025-10-24 14:56:05 +0100

    Added origin for CORS

--- src/configuration/dbConfig.js
+++ src/configuration/dbConfig.js
@@ -1,18 +1,3 @@
-// const mongoose = require("mongoose");
-
-// mongoose.connect("mongodb://localhost:27017/jwt_db")
-
-// mongoose.connection.on("connected", () => {
-//     console.log(`Mongodb Database successfully connected`)
-// })
-
-// mongoose.connection.on("error", (error) => {
-//     console.error(`Error connecting to Mongodb database due to ${error.message}`)
-// })
-
-
-// module.exports = mongoose
-
 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv")
--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000   
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://localhost:5173", "https://jwt-frontend-sandy.vercel.app"],
+    credentials: true
+}));
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
